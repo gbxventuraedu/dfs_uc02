d16f9845b4a06dc30987ba8718562b586b4d9daa
gbxventuraedu <gabriel.ventura@senacsp.edu.br>
2024-04-01 14:17:36 -0300

    commit js

---
+++ 05 - Inicio-JavaScript/01-04-js/js/script.js
@@ -0,0 +1,21 @@
+console.log("Olá mundo!");
+console.log('Olá mundo 2');
+console.log(`Olá mundo ${(1*3)}`);
+
+// Exemplo variável
+let nome = 'Gabriel';
+const idade = 38;
+var profissao = 'programador'
+
+// exemplo console.log 01
+console.log(nome + idade +profissao);
+
+// exemplo console.log 02
+console.log(`${nome} ${idade} ${profissao}`);
+
+// exemplo console.log 03
+console.log(`Olá ${nome}, sua idade é ${idade}. Você é um ${profissao}.`);
+
+// exemplo console.log 04
+console.log("Usuário:" + nome + "\n Idade: " + idade + "\n Profissão: " + profissao);
+
