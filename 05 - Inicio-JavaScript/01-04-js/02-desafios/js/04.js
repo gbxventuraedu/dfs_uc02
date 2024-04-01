9e80d465fe4bae7b91e06839227b4b350a1ffab8
gbxventuraedu <gabriel.ventura@senacsp.edu.br>
2024-04-01 16:39:15 -0300

    commit resultados

--- 05 - Inicio-JavaScript/01-04-js/02-desafios/js/04.js
+++ 05 - Inicio-JavaScript/01-04-js/02-desafios/js/04.js
@@ -1,6 +1,7 @@
 //4. Escreva um programa em JavaScript que solicite um número ao usuário e exiba a tabuada desse número de 1 a 10. Utilize uma estrutura de repetição while ou for para calcular e exibir a tabuada do número informado pelo usuário. O resultado deve ser exibido no console.
-let n1 = parseInt(prompt("Digite um número: "));
 
+let n = parseInt(prompt('Digite um número: '));
 for (let i = 0; i < 11; i++) {
-  console.log(`${n1} x ${i} = ${n1 * i}`);
+  console.log(`${i} * ${n} = ${i * n}`);
+  
 }
