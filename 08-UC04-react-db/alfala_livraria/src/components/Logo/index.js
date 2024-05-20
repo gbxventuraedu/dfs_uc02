3d5f68c36723e6bc5a8976efe14f3b69d8e66e14
gbxventuraedu <gabriel.ventura@senacsp.edu.br>
2024-05-20 17:15:56 -0300

    Aula react novo

---
+++ 08-UC04-react-db/alfala_livraria/src/components/Logo/index.js
@@ -0,0 +1,15 @@
+import logo from "../../img/logo.svg";
+import "./style.css";
+
+function Logo() {
+  return (
+    <div className="logo">
+      <img src={logo} alt="Alfala logo" className="logo-img" />
+      <p>
+        <strong>Alfala</strong>Books
+      </p>
+    </div>
+  );
+}
+
+export default Logo;
