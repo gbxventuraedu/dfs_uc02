a2879d66570a6d65929e7b2e7535016988a4039f
gbxventuraedu <gabriel.ventura@senacsp.edu.br>
2024-06-03 17:12:40 -0300

    aula 03-06

--- 08-UC04-react-db/alfala_livraria/src/App.js
+++ 08-UC04-react-db/alfala_livraria/src/App.js
@@ -1,14 +1,19 @@
-import "./App.css";
 import Header from "./components/Header";
+import styled from "styled-components";
 
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(180deg, #002f52 40%, #326589 165%);
+  .content {
+    min-height: 100vh;
+  }
+`;
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <Header />
-      <div className="content">
-        <p>sopdjfosdifjosdifjosidfjoisdj</p>
-      </div>
-    </div>
+    </AppContainer>
   );
 }
 
